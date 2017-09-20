8306027d587fff3dae84381bcd3b60ea9bb53b00
jeet bafna <jeetanil97@gmail.com>
2017-09-20 19:20:53 +0530

    Cart with paypal

--- controllers/cart.js
+++ controllers/cart.js
@@ -21,4 +21,27 @@ module.exports = function(router){
 			cart: displayCart
 		});
 	});
+	router.post('/:id', function(req, res){
+		req.session.cart = req.session.cart || {};
+		var cart = req.session.cart;
+
+		Book.findOne({_id:req.params.id}, function(err, book){
+			if(err){
+				console.log(err);
+			}
+
+			if(cart[req.params.id]){
+				cart[req.params.id].qty++
+			} else {
+				cart[req.params.id] = {
+					item: book._id,
+					title: book.title,
+					price: book.price,
+					qty: 1
+				}
+			}
+
+			res.redirect('/cart');
+		});
+	});
 }
